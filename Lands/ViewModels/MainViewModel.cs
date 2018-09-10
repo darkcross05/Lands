177bd697b2bc2a5666f8c31cb73f4c451ad76d91
Juan <juan@MacBook-Pro.local>
2018-09-09 21:54:06 -0500

    Second Part

--- Lands/ViewModels/MainViewModel.cs
+++ Lands/ViewModels/MainViewModel.cs
@@ -3,6 +3,19 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
-        
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
